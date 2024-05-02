a98001cfc74e225b066a0b8480a65651fa484880
github-usman <usmantoyou@gmail.com>
2024-05-02 15:57:10 +0530

    accessible-affortdale-adaptable : complete and icons fix of get-you-virtual-office

--- components/landing-page/GetYourVirtualOffice.tsx
+++ components/landing-page/GetYourVirtualOffice.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import connect_with_regi from "../../assets/icons/landing-page/get-you-virtual-office/connect_with_regi.png";
+import connect_with_regi from "../../assets/icons/landing-page/get-you-virtual-office/connect_with_regi.svg";
 import get_your_virtual_office from "../../assets/icons/landing-page/get-you-virtual-office/get_your_virtual_office.svg";
 import give_essential_details from "../../assets/icons/landing-page/get-you-virtual-office/give_essential_details.svg";
 import des_top_arrow from "../../assets/icons/landing-page/get-you-virtual-office/des_top_arrow.svg";
@@ -12,20 +12,19 @@ const GetYourVirtualOffice = () => {
     <div id="GetYourVirtualOffice" className="get-your-virtual-office">
       <div className="container ">
         <div className="heading">
-          <Image src={connect_with_regi} alt="hello"/>
           <h2>Get Your Virtual Office Up and Running in <span>3 Easy Steps!</span></h2>
           <p>Procedure to obtain Virtual office address.</p>
         </div>
         <div>
         <div className="details-section">
               <div className="all-details one">
-                <Image src={get_your_virtual_office} alt="icon" loading="lazy"/>
+                <Image src={connect_with_regi} alt="icon" loading="lazy"/>
                 <h5>Connect With RegisterKaro</h5>
                 <p>Reach out to our friendly customer support team through chat, email, or phone. They'll guide you through the process.</p>
               </div>
               <Image src={des_top_arrow} className="arrows top"  alt="arrow top"/>
               <div className="all-details two">
-                <Image src={get_your_virtual_office} alt="icon" loading="lazy"/>
+                <Image src={give_essential_details} alt="icon" loading="lazy"/>
                 <h5>Give Essential Details & Documents</h5>
                 <p>Upload essential documents required for registration, following our clear and secure guidelines.</p>
               </div>
--- pages/landing-page.tsx
+++ pages/landing-page.tsx
@@ -3,6 +3,7 @@ import Header from "../components/HomePage/Header";
 import { Footer } from "../components/Footer/Footer";
 import Faq from "../components/landing-page/Faq";
 import GetYourVirtualOffice from "../components/landing-page/GetYourVirtualOffice";
+import AccessibleAffordableAdaptable from "../components/landing-page/AccessibleAffordableAdaptable";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <NextNProgress color="#ffa229" height={5} />
          {/* <Header /> */}
          <GetYourVirtualOffice/>
+         <AccessibleAffordableAdaptable/>
          <Faq />
         <Footer></Footer>
       </>
