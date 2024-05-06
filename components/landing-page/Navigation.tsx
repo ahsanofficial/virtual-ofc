0b7dc62f9a9d8d6522859bc6f4fa20df9db011ac
codingpartnerNB <nehab5314@gmail.com>
2024-05-06 13:08:37 +0530

    Neha : Fixing logo size

--- components/landing-page/Navigation.tsx
+++ components/landing-page/Navigation.tsx
@@ -14,8 +14,9 @@ const Navigation = () => {
         alt="Register Karo Logo"
         objectFit="cover"
         objectPosition="center"
-        height={40}
-        width={225}
+        className="logo-image"
+        height={88.54}
+        width={123.72}
       />
       <div className="navigation-links">
         <nav>
