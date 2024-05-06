0cc1c9df9eb3570c8238180a0cc7e45708f17431
ahsanofficial <bega@gmail.com>
2024-05-06 18:45:23 +0530

    remove prisma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,8 @@ import "../public/assets/styles/main.scss";
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <html lang='en'>
-      <meta property="og:site_name" content="registerkaro" />
+      <meta property="og:site_name" content="launchwise" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://www.registerkaro.in/_next/static/media/Logo.77348f99.png" />
       <link rel="icon" href="/favicon.ico" />
       {/* <Header /> */}
       <Component {...pageProps} />
