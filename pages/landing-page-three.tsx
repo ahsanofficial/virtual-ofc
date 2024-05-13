653aaf61f603ccc4369eec14a52018eb837fff5b
github-usman <usmantoyou@gmail.com>
2024-05-13 22:48:14 +0530

    add new content : free download

--- pages/landing-page-three.tsx
+++ pages/landing-page-three.tsx
@@ -1,11 +1,12 @@
 import Benefits from "../components/landing-page-three/Benefits";
-
+import FreeDownload from "../components/landing-page-three/FreeDownload"
 
 export default function Home() {
   return (
     <div>
       <>
         <Benefits/>
+        <FreeDownload/>
       </>
     </div>
   );
