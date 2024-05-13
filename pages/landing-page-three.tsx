48aa6b80d5914f31975d653c770f4a023f1d4d25
github-usman <usmantoyou@gmail.com>
2024-05-13 21:33:33 +0530

    add new section: benefits

---
+++ pages/landing-page-three.tsx
@@ -0,0 +1,12 @@
+import Benefits from "../components/landing-page-three/Benefits";
+
+
+export default function Home() {
+  return (
+    <div>
+      <>
+        <Benefits/>
+      </>
+    </div>
+  );
+}
