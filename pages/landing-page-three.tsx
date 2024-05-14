73d113fe157cb009de44558d94001acbb5e6952d
codingpartnerNB <nehab5314@gmail.com>
2024-05-14 13:54:28 +0530

    Neha : Adding Header and Hero of landing page three

---
+++ pages/landing-page-three.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/landing-page-three/Header'
+import Hero from '../components/landing-page-three/Hero';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
